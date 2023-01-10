cc458a21f6fa1ac12d07e852d2fd8a34c68a18bd
saichandrasekar <saichandrasekar@gmail.com>
2023-01-10 09:46:39 +0530

    added search and result

--- api.py
+++ api.py
@@ -1,4 +1,8 @@
+# Runs as : flask --app api run --host=0.0.0.0
+
+
 from flask import Flask, render_template, request
+import time as time_
 
 app = Flask(__name__)
 
@@ -10,5 +14,18 @@ def hello_world():
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files['the_file']
-        f.save('./test-files/'+uploaded_file.txt')
+        f = request.files['input_image']
+        new_file_name = str(round(time_.time() * 1000)) + '_' + f.filename
+
+        print("New file Name :", new_file_name)
+        f.save('./../test_files/'+new_file_name)
+
+        response =  {
+            'status': 'success',
+            'fileName' : new_file_name
+        }
+        return render_template('search.html', response = new_file_name)
+
+@app.route("/search", methods=['GET'])
+def search_image():
+    return render_template('result.html')
