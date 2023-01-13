4bb8c2690be480219360cacec43e045b86c4a32f
saichandrasekar <saichandrasekar@gmail.com>
2023-01-13 20:32:47 +0530

    test secret

--- service.py
+++ service.py
@@ -9,8 +9,8 @@ import cv2
 # hash dictionary to store hash values on images dyrdytd
 image_hash_dict = {}
 
-aws_access_key_id=JHFDJHFJHD
-aws_secret_access_key=58757858KJGVKJVKJFHK879587587*&%&*%%
+aws_access_key_id='JHFDJHFJHD'
+aws_secret_access_key='58757858KJGVKJVKJFHK879587587*&%&*%%'
 
 
 def hash_array_to_hash_hex(hash_array):
